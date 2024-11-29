9219bf600b5afe25314ef59cd92c5fab2d65f0aa
Dima Zajcew PC <dzx01@proton.me>
2024-11-30 00:05:06 +0100

    initial commit

---
+++ MVC first app/MVC first app/Data/MVC_first_appContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_first_app.Models;
+
+namespace MVC_first_app.Data
+{
+    public class MVC_first_appContext : DbContext
+    {
+        public MVC_first_appContext (DbContextOptions<MVC_first_appContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_first_app.Models.Movie> Movie { get; set; } = default!;
+    }
+}
